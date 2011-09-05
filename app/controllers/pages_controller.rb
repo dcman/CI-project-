6945f35f63fa196862bc448227d78b3ef76daf66
Justin Johnson <Justin.johnson.90@my.csun.edu>
2011-09-04 22:30:53 -0700

    html done I think

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,57 @@ def index
   @title = "Welcome"
   respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @questions }
+      format.xml  
     end
 end
+def about
+  @title = "About US"
+  respond_to do |format|
+    format.html
+    format.xml 
+  end
+end
+def services 
+  @title = "Services"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+def testimony
+  @title = "Testimony"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+def charters
+  @title = "Charters"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+def contact
+  @title = "Contact"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+def directions
+  @title = "Directions"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+def faq
+  @title = "FAQ"
+  respond_to do |format|
+    format.html 
+    format.xml
+  end
+end
+
 end
