ec3c92ba28aaaca67d3544695cb877ca9062f306
Justin Johnson <Justin.johnson.90@my.csun.edu>
2011-09-29 13:14:54 -0700

    can make new admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,9 +1,21 @@
 class AdminsController < ApplicationController
   def new
-    @title = "New Admin"
+    @title = "Admin"
+    @admin = Admin.new
   end
+
   def show
     @title = "Admin"
     @admin = Admin.find(params[:id])
+  end
+
+  def create
+    @admin = Admin.new(params[:admin])
+    if @admin.save
+      redirect_to admins_path
+      else
+      @title = "Admin"
+      render 'new'
+    end
   end
 end
