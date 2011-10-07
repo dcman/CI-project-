9ef21ba27bdbaef65b3447907a54f3823bded6c0
Justin Johnson <Justin.johnson.90@my.csun.edu>
2011-10-07 14:31:21 -0700

    fixing trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,29 @@
-puts 'creating admin'
-@admin = Admin.create(:name => "Setup", :email => "setup@example.com", 
-:password => 'setuppassword'
-)
-if @admin.save!
-  puts "#{@admin.name} was created"
+@admin = Admin.find_by_email("setup@example.com")
+if @admin
+  puts "Admin setup complete"
 else
-  puts "#{@admin.name} Was not created."
+  puts 'Seting up admin'
+  @admin = Admin.create(:name => "Setup", :email => "setup@example.com",
+:password => 'setuppassword'
+  )
+  if @admin.save!
+    puts "#{@admin.name} was created"
+  else
+    puts "#{@admin.name} Was not created."
+  end
 end
 
+@trip = Trip.find_by_name("Setup")
+if @trip
+  puts "Trips setup complete"
+else
+  puts 'Seting up trip'
+  @trip = Trip.new(:name => "Setup", :date => "12/30/2000",
+  :description => "This is what a trip should look like", :sum =>"1236549870")
+
+  if @trip.save!
+    puts "#{@trip.name} was created"
+  else
+    puts "#{@trip.name} was not created"
+  end
+end
