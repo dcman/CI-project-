3d17ae6fe28261f3f7d5c84c8a2eead0161558d0
Justin Johnson <Justin.johnson.90@my.csun.edu>
2011-10-07 11:52:20 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+hash = [:name => "Justin", :email => "srwaxalot@gmail.com", 
+  :encrypted_password => "97b28a8f516d21ce0524c8536bfcf2d3b9ddc7269c677c8e789677bf9c918cfd"
+  :salt => "615c00fcf2e5b720243883a711e2026fb23b5de8f1343adaf4b1e9a05dac1f75"]
+puts 'creating admin'
+admin = Admin.create(hash)
+if admin.save
+  puts 'saved'
+else 
+  pust 'faild'
+end
