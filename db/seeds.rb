e1f15bd793c3813b731c9ff471ca56a175b6fbfb
Justin Johnson <Justin.johnson.90@my.csun.edu>
2011-10-08 01:00:49 -0700

    seed seems to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,44 @@ else
     puts "#{@trip.name} was not created"
   end
 end
+@survey = @trip.survey
+if @survey
+  puts "#{@survey.name} was created"
+else
+  @survey = Survey.new(:name => "Sample survey", :date => (@trip.date), :trip_id => (@trip.id))
+  if @survey.save!
+    puts "#{@survey.name} was created"
+  else
+    puts "#{@survey.name} was not created"
+  end
+end
+
+if @trip.particeipant.size == 0
+  @particeipant = @trip.particeipant.new(:email => 'Test@test.com', :first_name => "Setup", :last_name => "Test")
+  if @particeipant.save!
+    puts "#{@particeipant.first_name} was created"
+  else
+    puts "#{@particeipant.first_name} was not created"
+  end
+end
+
+if @survey.question.all.size >= 3
+  puts "Questions setup complete"
+else
+(1..3).each do |n|
+    @question = Question.new(:question => "What is the answer to question #{n} ?",
+    :survey_id => (@survey.id), :q_n => (n))
+    @answer = Answer.new(:answer => "This is my answer for question #{n}", :particeipant_id => (@particeipant.id),
+    :q_n_id =>(@question.q_n) )
+    if @question.save!
+      puts "Question #{n} was created"
+    else
+      puts "Question #{n} was not created"
+    end
+    if @answer.save!
+      puts "Answer #{n} was created"
+    else
+      puts "Answer #{n} was not created"
+    end
+  end
+end
